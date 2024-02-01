04d876059b413f3c5b718195a7ac0af8e9b33661
Santiago Diaz Muñoz <149324836+Diaz1010@users.noreply.github.com>
2024-02-01 16:02:02 +0000

    Final Domain hack

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 
 //Arrays//
-let initial=["your","git","meta","open","nitro"];
-let midle=["space","book","tube","hub","earth"];
-let domain=["inter.net","matr.ix","doma.in",".exe"];
+let initial = ["you", "git","nitro"];
+let midle = ["space", "tube","hub"];
+let domain = ["internet", "matrix", "puedes"];
+let extension = [".com", ".es"];
 
-//Domain Array//
-let FullArray=[];
-//fors anidados//
+// Domain Array//
+let FullArray = [];
+
+// Fors anidados//
 for (let i = 0; i < initial.length; i++) {
     for (let m = 0; m < midle.length; m++) {
         for (let d = 0; d < domain.length; d++) {
-            let FullDomain=initial[i]+midle[m]+domain[d];
-            FullArray.push(FullDomain);
-            
-           
-            
-        }     
-    }    
+            for (let h = 0; h < extension.length; h++) {
+                let domainName;
+                let extensionHack = extension[h].replace(".", "");
+                if (domain[d].endsWith(extensionHack)) {
+                    let modifiedDomain = domain[d].slice(0, -extensionHack.length);
+                    domainName = initial[i] + midle[m] + modifiedDomain + extension[h];
+                } else {
+                    domainName = initial[i] + midle[m] + domain[d] + extension[h];
+                }
+                FullArray.push(domainName);
+            }
+        }
+    }
 }
 console.log(FullArray);
-
- 
